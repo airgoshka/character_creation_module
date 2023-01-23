3d250caf1b8a055accd546fa55e29edf8328f415
Игорь Андреев <airgoshka@yandex.ru>
2023-01-23 18:51:31 +0300

    Тема5 Урок7

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
+    """
+    Тренировка атаки
+    Передаваемые параметры:
+    char_name - имя игрока,
+    char_class - класс игрока.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +22,12 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """
+    Тренировка защиты
+    Передаваемые параметры:
+    char_name - имя игрока,
+    char_class - класс игрока.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +38,12 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """
+    Тренировка специальной атаки
+    Передаваемые параметры:
+    char_name - имя игрока,
+    char_class - класс игрока.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость '
                 f'{80 + 25}»')
@@ -38,6 +57,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """
+    Начать тренироку
+    Передаваемые параметры:
+    char_name - имя игрока,
+    char_class - класс игрока.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +87,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор имени и класса персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
